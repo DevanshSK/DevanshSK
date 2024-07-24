831198f0d6b21ab7669f0a39e51f297f75a54fdb
DevanshSK <devanshs843@gmail.com>
2024-07-24 18:08:28 +0530

    Added projects section with animated cards.

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,19 +1,23 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
+import dynamic from "next/dynamic";
 import { FaHome } from "react-icons/fa";
+
+// import RecentProjects from "@/components/RecentProjects";
+const RecentProjects = dynamic(() => import('@/components/RecentProjects'), { ssr: false })
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
-          navItems={[
-            {name: "Hone", link: "/", icon: <FaHome />}
-          ]} 
+          navItems={navItems} 
         />
         <Hero />
-        <Grid />
+        
+        <RecentProjects />
       </div>
     </main>
   );
